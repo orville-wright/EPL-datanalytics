7c351d6d2a0ef9ca2292f7f56bdd88394b182e38
orville-wright <orville.wright@yahoo.com>
2015-08-21 20:36:30 -0700

    new gameweek counte code

--- tutorial/tutorial/spiders/dmoz_spider_fixtures.py
+++ tutorial/tutorial/spiders/dmoz_spider_fixtures.py
@@ -1,4 +1,5 @@
 import sys
+import re
 #from scrapy.spider import BaseSpider
 #from scrapy.spider import CrawlSpider
 #
@@ -10,46 +11,33 @@ from scrapy.http import HtmlResponse
 
 from tutorial.items import DmozItem
 
-gw = 20
 class fixturesSpider(Spider):
    name = "fixtures"
    allowed_domains = ["premierleague.com"]
 
    def start_requests(self):
-      for u in range(1, 7):
+      for u in range(3, 7):
          gw = u
          print "SCANNING gameweek: [ %d ] URL: [ %d ]..." % (gw, u)
          yield Request('http://fantasy.premierleague.com/fixtures/' + str(u) +'/', self.parse)
 
    def parse(self, response):
       sel0 = Selector(response)
+      gw_url = response.url
+      gw = int(re.sub(r'\D', "", gw_url))
       print "GETTING fixture data for gameweek: [ %d ]" % (gw)
-      xx = getfixdata(sel0, gw)
-      print "XX = ", xx
-      xx+=1
-
-# set xpath point to extract dates & times within the "ism" TR block
-# getting the list of dates tells us...
-# 1. A count of games that will be played in this weeks gameweek
-# 2. the date and time of each game is
-
-def getfixdata(selector, gw):
-    sel0 = selector
-    xx = gw
-    fixchunk = sel0.xpath('//*[@id="ism"]//tr[@class="ismFixture"]//td[1]/text()')
-    fixtures = []
-    for j, k in enumerate(fixchunk.extract()):
-        item = DmozItem()
-        a = gethometeam(sel0, j)
-        b = getawayteam(sel0, j)
-        item['fixdatetime'] = k
-        item['fixhometeam'] = a
-        item['fixawayteam'] = b
-        print "GAMEWEEK: %d - Game: %d starts at: %s - (Home) %s v %s (Away)" % (gw, j+1, k, a, b)
-        fixtures.append(item)
-    #return fixtures
-    xx+=1
-    return xx
+      fixchunk = sel0.xpath('//*[@id="ism"]//tr[@class="ismFixture"]//td[1]/text()')
+      fixtures = []
+      for j, k in enumerate(fixchunk.extract()):
+         item = DmozItem()
+         a = gethometeam(sel0, j)
+         b = getawayteam(sel0, j)
+         item['fixdatetime'] = k
+         item['fixhometeam'] = a
+         item['fixawayteam'] = b
+         print "GAMEWEEK: %d - Game: %d starts at: %s - (Home) %s v %s (Away)" % (gw, j+1, k, a, b)
+         fixtures.append(item)
+      return
 
 def gethometeam(selector, fixnum):
     homechunk = selector.xpath('//*[@id="ism"]//tbody//tr[@class="ismFixture"]//td[@class="ismHomeTeam"]/text()')
