5938fe9108928d7c380d3684ee00a9dccf513cac
orville-wright <orville.wright@yahoo.com>
2015-08-29 22:09:18 -0700

    add new code for user/password login

--- eplda/eplda/spiders/myteam_spider.py
+++ eplda/eplda/spiders/myteam_spider.py
@@ -1,5 +1,6 @@
 import sys
 import re
+import logging
 #
 import scrapy
 from scrapy.spider import Spider
@@ -7,57 +8,49 @@ from scrapy.selector import Selector
 from scrapy.http import Request
 from scrapy.http import HtmlResponse
 from eplda.items import DmozItem
+#
 
 class geteamnameSpider(Spider):
    name = "geteamname"
    allowed_domains = ["premierleague.com"]
-   start_urls = ["https://users.premierleague.com/PremierUser/account/login.html",
-   #start_urls = ["https://fantasy.premierleague.com",
-                "http://fantasy.premierleague.com/my-team/"
-                ]
+   start_urls = ['http://fantasy.premierleague.com']
 
    #def start_requests(self):
+   # this is incomplete over-ride code for start_urls object, which I hate using.
    #    print "REQUESTING login URL form..."
-   #    #yield Request('https://users.premierleague.com/PremierUser/account/login.html', self.parse)
+   #    yield Request('https://users.premierleague.com/PremierUser/account/login.html', self.parse)
    #    yield Request('http://fantasy.premierleague.com/', self.parse)
 
    def parse(self, response):
-       print "Setting up login form credentials and auto input using URL: %s" % (response.url)
-       #return scrapy.FormRequest.from_response(
-       return scrapy.FormRequest.from_response(
-           response,
-           formdata={'j_username': 'orville.wright@yahoo.com', 'j_password': 'sanfran1'},
-           #formdata={'ismEmail': 'orville.wright@yahoo.com', 'id_password': 'sanfran1'},
-           #formdata={'ismEmail': 'orville.wright@yahoo.com', 'password': 'sanfran1'},
-           #formdata={'email': 'orville.wright@yahoo.com', 'id_password': 'sanfran1'},
-           #formdata={'email': 'orville.wright@yahoo.com', 'password': 'sanfran1'},
-           dont_click=True,
-           #callback=self.after_login
-           callback=self.get_myteam
-      )
-
-   def after_login(self, response):
-        # check login succeed before going on
-        print "CHECKING login credentials post status..."
-        #print "RESPONSE BODY: %s" % (response.body)
-        #if "Error logging in" in response.body:
-        #    self.logger.error("Login failed")
-        #    print "LOGIN credentials failure !!"
-        #    return
-        #print "LOGIN credentials entered SUCCESSFULLY !!"
-        #response = scrapy.Request('http://fantasy.premierleague.com/my-team/')
-
+       # this is incorrect now that only main login URL is being passed
+       # either delete it or clean it up
+       print "*** MAIN: URL is:  %s" % (response.url)
+       if response.url == "http://fantasy.premierleague.com/my-team/":
+          print "*** SUCESS skipping credentials code..."
+          return
+       else:
+          print "*** Setting up LOGIN form using URL: %s" % (response.url)
+          return scrapy.FormRequest.from_response(
+             response,
+             formdata={'email': 'trashcan_x@yahoo.com', 'password': 'sanfran1'},
+             dont_click=True,
+             #callback=self.after_login
+             callback=self.get_myteam
+          )
+       print "*** UNKNOWN STATE ***"
+       return
 
    def get_myteam(self, response):
-        print "GETTING team name - Old URL %s" % (response.url)
-        print response.body
-        #response2 = scrapy.Request.replace(response.request, url="http://fantasy.premierleague.com/my-team/")
-        #print "GETTING team name - New URL %s" % (response2.url)
-        sel0 = Selector(response)
-        #sel1 = Selector(response2)
-        print "GETTING team name..."
-        #datachunk = sel0.xpath('//*[@id="ismTeamForm"]/div[1]/div[1]/div[1]/h2/text()')
-        myteamname = sel0.xpath('//*[@id="ismTeamForm"]/div[1]/div[1]/div[1]/h2/text()').extract()
-        #my-teamname = datachunk.extract()
-        print "TEAM NAME is: %s" % (myteamname)
+        if response.url == "http://fantasy.premierleague.com/?fail":
+           print "*** ABORTING as we are not logged in yet"
+        else:
+           print "*** LOGGED IN get_myteam using URL %s" % (response.url)
+           print "*** GETTING team info from URL: %s" % (response.url)
+           sel0 = Selector(response)
+           print "*** GETTING team info..."
+           user_uuid = response.url.split('/')
+           print "Player unique uid: %s" % (user_uuid[4])
+           print "Player name; %s" % (sel0.xpath('//*[@id="ism"]/section[2]/h1/text()').extract())
+           print "Team name; %s" % (sel0.xpath('//*[@id="ism"]/section[2]/div[1]/h2/text()').extract())
+           print "Gameweek points: %s" % (sel0.xpath('//*[@id="ism"]/section[2]/div[2]/div[2]/dl/dd[4]/a/text()').extract())
         return
